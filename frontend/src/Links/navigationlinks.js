f135e96c4280a4d7671f7ade7d6ed5412229c4d8
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-06-13 10:20:22 +0530

    added navigation links

---
+++ frontend/src/Links/navigationlinks.js
@@ -0,0 +1,29 @@
+const home = {
+    title: 'Home',
+    link: '/'
+}
+const about = {
+    title: 'About',
+    link: '/about'
+}
+const services = {
+    title: 'Services',
+    link: '/services'
+}
+const contact = {
+    title: 'Contact',
+    link: '/contact'
+}
+const projects = {
+    title: 'Projects',
+    link: '/projects'
+}
+const navigationlinks = [
+
+    home,
+    about,
+    services,
+    contact,
+    projects
+]
+export default navigationlinks;

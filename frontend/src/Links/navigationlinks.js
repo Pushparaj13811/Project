39affa7249939d90c768cb799c7daf8e5f5ab883
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-06-14 22:41:53 +0530

    Updated some links

--- frontend/src/Links/navigationlinks.js
+++ frontend/src/Links/navigationlinks.js
@@ -6,24 +6,16 @@ const about = {
     title: 'About',
     link: '/about'
 }
-const services = {
-    title: 'Services',
-    link: '/services'
-}
+
 const contact = {
     title: 'Contact',
     link: '/contact'
 }
-const projects = {
-    title: 'Projects',
-    link: '/projects'
-}
+
 const navigationlinks = [
 
     home,
     about,
-    services,
-    contact,
-    projects
+    contact
 ]
 export default navigationlinks;

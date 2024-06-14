7a8e94e4d25ba90d79858ce9c3cd3b3f12a80a70
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-06-14 22:39:48 +0530

    added different assets

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,5 +1,29 @@
-import closedEye from '../assets/closedEye.svg';
-import openEye from '../assets/openEye.svg';
-import favicon from '../assets/favicon.svg';
+import closedEye from "../assets/closedEye.svg";
+import openEye from "../assets/openEye.svg";
+import favicon from "../assets/favicon.svg";
+import profile from "./profile.jpg";
+import lightsaved from "./light-saved.png";
+import darksaved from "./dark-saved.png";
+import archive from "./archives.svg";
+import darktagged from "./dark-tagged.png";
+import lighttagged from "./light-tagged.png";
+import lightscroll from "./light-scroll.svg";
+import darkscroll from "./dark-scroll.svg";
+import darkstory from "./story-dark.png";
+import lightstory from "./story-light.png";
 
-export { closedEye,openEye, favicon };
+export {
+  closedEye,
+  openEye,
+  favicon,
+  profile,
+  lightsaved,
+  darksaved,
+  lightscroll,
+  darkscroll,
+  archive,
+  darktagged,
+  lighttagged,
+  darkstory,
+  lightstory,
+};

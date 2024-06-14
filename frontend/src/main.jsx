725409d4c9e1c07e0acd830fc5c305c3e8412ea7
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-06-14 11:36:41 +0530

    Configure routing for 404 error page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,15 +4,19 @@ import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Root from './Root'
 import SignUP from './components/SignUp/SignUP'
+import Login from './components/Login/Login'
 import Home from './components/Home/Home'
-import Navbar from './components/Navbar/Navbar'
+import ForgotPassword from './components/Forgotpassword/ForgotPassword'
+import NotFound from './components/Notfound/NotFound'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route path="" element={<Home />}></Route>
       <Route path="/signup" element={<SignUP />}></Route>
-
+      <Route path="/login" element={<Login />}></Route>
+      <Route path='/forgotpassword' element={<ForgotPassword />}></Route>
+      <Route path='*' element={<NotFound />} />
     </Route>
   )
 )
@@ -22,7 +26,7 @@ const router = createBrowserRouter(
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-      <RouterProvider router={router} />
-  
+    <RouterProvider router={router} />
+
   </React.StrictMode>,
 )

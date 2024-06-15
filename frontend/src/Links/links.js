b1e808bd55f263880b7aaa576a645ed9774875f0
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-06-15 17:36:21 +0530

    Updated links

---
+++ frontend/src/Links/links.js
@@ -0,0 +1,28 @@
+import { video } from "@/assets";
+const links = [
+  {
+    content:
+      "https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&w=800",
+    desc: "A beautiful sunset",
+    linktype: "image",
+  },
+  {
+    content:
+      "https://images.pexels.com/photos/919278/pexels-photo-919278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "A beautiful sunset",
+    linktype: "image",
+  },
+  {
+    content: video,
+    desc: "A beautiful volcano",
+    linktype: "video",
+  },
+  {
+    content:
+      "https://images.pexels.com/photos/1580271/pexels-photo-1580271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "A beautiful sunset",
+    linktype: "image",
+  },
+];
+
+export default links;
